c7437187b2675d9142d2034c21308c878a848f30
Troy Toggweiler <ttoggweiler@fit.edu>
2017-02-15 18:47:38 -0500

    init classes

---
+++ code/CSE5693HW2/src/main/java/com/ttoggweiler/cse5693/learner/TreeBuilder.java
@@ -0,0 +1,8 @@
+package com.ttoggweiler.cse5693.learner;
+
+/**
+ * Created by ttoggweiler on 2/15/17.
+ */
+public class TreeBuilder
+{
+}
---
+++ code/CSE5693HW2/src/main/java/com/ttoggweiler/cse5693/tree/Feature.java
@@ -0,0 +1,8 @@
+package com.ttoggweiler.cse5693.tree;
+
+/**
+ * Created by ttoggweiler on 2/15/17.
+ */
+public class Feature
+{
+}
