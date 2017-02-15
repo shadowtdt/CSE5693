40d1eef5232ab1422508bfa6242098c8dcaff973
Troy Toggweiler <ttoggweiler@fit.edu>
2017-02-15 18:15:22 -0500

    hw2 init

---
+++ code/CSE5693HW2/src/main/java/com/ttoggweiler/cse5693/DecisionTreeRunner.java
@@ -0,0 +1,18 @@
+package com.ttoggweiler.cse5693;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class that runs CSE5693 - HW2
+ * Handles program arguments and sets up necessary libraries
+ */
+public class DecisionTreeRunner
+{
+    private static Logger log = LoggerFactory.getLogger(DecisionTreeRunner.class);
+    public static void main(String[] args)
+    {
+        log.info("=== CSE5693-Hw2 Decision Tree Runner ====");
+    }
+}
